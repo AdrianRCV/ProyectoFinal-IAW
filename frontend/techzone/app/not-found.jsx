1430f468669f4f587f5cdcd5bc1365929d98e8ec
raul595 <raulcalin15@gmail.com>
2025-02-25 09:51:43 +0100

    react instalado

---
+++ frontend/techzone/app/not-found.jsx
@@ -0,0 +1,17 @@
+"use client"
+import { useEffect } from "react"
+
+export default function Error({ error, reset }) {
+     useEffect(() => {
+         console.error(error)
+     },[error])
+
+    return (
+        <div>
+            <h2>pagina de error!</h2>
+            <button onClick={reset}>
+                 Try again
+            </button>
+        </div>
+    );
+}
