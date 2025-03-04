c09637c2859e8156e21d14cb195d8002352bb3f3
AdrianRCV <adriancamposvargas4@gmail.com>
2025-03-04 14:23:56 +0100

    Solucionando conflictos 2

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -18,6 +18,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username }; // Retorna la información que quieras
+    return { userId: payload.sub, username: payload.username }; 
   }
 }
